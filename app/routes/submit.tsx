a8b26d60850347b40293c724a1233888b88e434c
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-18 12:38:42 +1300

    store previous answers in database

--- app/routes/submit.tsx
+++ app/routes/submit.tsx
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod.mjs";
 import { z } from "zod";
 import { systemPrompt } from "~/data/prompts";
+import { db } from "~/shared/db";
 import type { Route } from "./+types/submit";
 
 const countryResponse = z.object({
@@ -12,8 +13,24 @@ const countryResponse = z.object({
 export async function action({ request }: Route.ActionArgs) {
 	const formData = await request.formData();
 
-	const country = formData.get("country");
-	const suggestion = formData.get("suggestion");
+	const country = formData.get("country") as string;
+	const suggestion = (formData.get("suggestion") as string).toLowerCase();
+
+	const trivia = await db.trivia.findFirst({
+		where: {
+			country: {
+				name: country,
+			},
+			userSuggestion: suggestion,
+		},
+	});
+
+	if (trivia) {
+		return Response.json({
+			pass: trivia.pass,
+			response: trivia.response,
+		});
+	}
 
 	const openai = new OpenAI({
 		apiKey: process.env.OPENAI_API_KEY,
@@ -34,6 +51,21 @@ export async function action({ request }: Route.ActionArgs) {
 	const res = JSON.parse(completion.choices[0].message.content!);
 
 	const parsedRes = countryResponse.parse(res);
+
+	const countryObj = await db.country.findFirst({
+		where: {
+			name: country,
+		},
+	});
+
+	const newTrivia = await db.trivia.create({
+		data: {
+			countryId: countryObj?.id!,
+			userSuggestion: suggestion,
+			pass: parsedRes.pass,
+			response: parsedRes.response,
+		},
+	});
 
 	return Response.json({
 		...parsedRes,
