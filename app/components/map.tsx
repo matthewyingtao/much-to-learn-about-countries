01d31f372df0fe0637da9a02535b644c59925397
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-24 16:39:02 +1300

    add map component

---
+++ app/components/map.tsx
@@ -0,0 +1,30 @@
+import { mapPaths } from "~/data/countries";
+
+export default function Map({
+	highlightCountry,
+}: {
+	highlightCountry: string;
+}) {
+	return (
+		<svg
+			fill="#ececec"
+			stroke="black"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth=".2"
+			version="1.2"
+			viewBox="0 0 2000 857"
+			className="h-full w-full"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			{mapPaths.map(({ path, name }) => (
+				<path
+					key={name}
+					d={path}
+					fill={highlightCountry === name ? "red" : "white"}
+					stroke={highlightCountry === name ? "red" : "black"}
+				/>
+			))}
+		</svg>
+	);
+}
