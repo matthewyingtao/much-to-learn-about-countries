3f805fae16110b1bba5eb3805193488f563ad49b
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-31 22:18:32 +1300

    add random country assignment function

--- app/shared/utils.ts
+++ app/shared/utils.ts
@@ -1,4 +1,16 @@
+import { $countriesAvailable, $currentCountry } from "./store";
+
 export function randomChoice<T>(arr: T[]): T {
 	const i = Math.floor(Math.random() * arr.length);
 	return arr[i];
+}
+
+export function assignRandomCountry() {
+	const newCountry = randomChoice($countriesAvailable.get());
+
+	$countriesAvailable.set(
+		$countriesAvailable.get().filter((country) => country !== newCountry)
+	);
+
+	$currentCountry.set(newCountry);
 }
