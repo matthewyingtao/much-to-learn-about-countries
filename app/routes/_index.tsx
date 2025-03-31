4037a2c7b25e2339b00b7d8faeceda14eb496768
Matthew Tao <matthew.yingtao@gmail.com>
2025-03-31 19:37:45 +1300

    add score display

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,7 +26,11 @@ export default function Home(_: Route.ComponentProps) {
 	const country = useStore($currentCountry);
 	const history = useStore($history);
 
-	const [countryCount, setCountryCount] = useState(0);
+	const [overallScore, setOverallScore] = useState(0);
+	const [attempts, setAttempts] = useState<boolean[]>([]);
+
+	const totalAttempts = history.length;
+	const currentScore = history.filter((item) => item.pass).length;
 
 	useEffect(() => {
 		$currentCountry.set(randomChoice(countries));
@@ -37,26 +41,28 @@ export default function Home(_: Route.ComponentProps) {
 
 		$history.set([...history, fetcher.data]);
 
-		if (!fetcher.data.pass && countryCount < 2)
-			setCountryCount((prev) => prev + 1);
+		setAttempts((prev) => [...prev, fetcher.data.pass]);
 
-		if (countryCount >= 2) {
+		if (currentScore >= 2) {
 			$currentCountry.set(randomChoice(countries));
-			setCountryCount(0);
+			setOverallScore((prev) => prev + 1);
+			setAttempts([]);
 		}
 	}, [fetcher.data]);
 
 	return (
 		<main className="grid grid-cols-[1fr_55ch] h-screen">
 			<Map highlightCountry={country} />
-			<div className="relative space-y-6 px-8 py-4 border-l border-black text-[#616161]">
+			<div className="relative px-8 py-4 border-l border-black text-[#616161]">
 				<img
 					src={pattern}
 					alt=""
 					className="absolute top-0 left-0 w-full -z-10 pointer-events-none"
 				/>
-				<h1 className="text-4xl w-[15ch]">Much to Learn About Countries</h1>
-				<fetcher.Form action="/api/submit" method="post">
+				<h1 className="text-4xl mb-6 w-[15ch]">
+					Much to Learn About Countries
+				</h1>
+				<fetcher.Form action="/api/submit" method="post" className="mb-4">
 					<h3 className="text-2xl">Current Country</h3>
 					<span className="block text-4xl leading-[0.8] pb-4">{country}</span>
 					{/* hidden form value */}
@@ -83,7 +89,9 @@ export default function Home(_: Route.ComponentProps) {
 					</div>
 				</fetcher.Form>
 
-				<h3 className="text-2xl">History</h3>
+				<ScoreDisplay attempts={attempts} />
+
+				<h3 className="text-2xl mb-4">History</h3>
 				<div className="flex flex-col space-y-4">
 					{history.map(({ country, suggestion, pass, response }, index) => (
 						<div
@@ -102,5 +110,45 @@ export default function Home(_: Route.ComponentProps) {
 				</div>
 			</div>
 		</main>
+	);
+}
+
+function ScoreDisplay({ attempts }: { attempts: boolean[] }) {
+	const totalAttempts = attempts.length;
+	const attemptsPadded: (boolean | undefined)[] =
+		attempts.length < 5
+			? [...attempts, ...Array(5 - attempts.length).fill(undefined)]
+			: attempts;
+
+	return (
+		<div className="flex gap-1 mb-6">
+			{attemptsPadded.map((attempt) => {
+				const gradientTable = {
+					pass: "from-[#C1E694] to-[#42A050]",
+					fail: "from-[#E69494] to-[#A04242]",
+					unattempted: "from-[#F8F8F8] to-[#E1E1E1]",
+				};
+
+				let gradientClass: string = "";
+
+				if (attempt === true) {
+					gradientClass = gradientTable.pass;
+				} else if (attempt === false) {
+					gradientClass = gradientTable.fail;
+				} else {
+					gradientClass = gradientTable.unattempted;
+				}
+
+				return (
+					<div
+						className={`grid place-items-center bg-gradient-to-t p-[2px] rounded-full overflow-clip ${gradientClass}`}
+					>
+						<div
+							className={`bg-gradient-to-b h-4 w-4 rounded-full ${gradientClass}`}
+						/>
+					</div>
+				);
+			})}
+		</div>
 	);
 }
