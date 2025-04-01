849c28413ee1f560ed4fa55cde1c6a0b616d277f
Matthew Tao <matthew.yingtao@gmail.com>
2025-04-02 01:21:24 +1300

    remove dev-server and prepare scripts; update netlify configuration for react-router integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-        input: "./server/app.ts",
-      }
-      : undefined,
-  },
-  optimizeDeps: {
-    include: ['openai']
-  },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-}));
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+});
