2ddbbf29516640e74c97975b6981f5937baab213
Matthew Tao <matthew.yingtao@gmail.com>
2025-04-02 10:05:27 +1300

    try prisma accelerate

--- app/shared/db.ts
+++ app/shared/db.ts
@@ -1,19 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-let db: PrismaClient;
+let db;
 
 declare global {
-	var __db__: PrismaClient | undefined;
+	var __db__: any;
 }
 
 if (process.env.NODE_ENV === "production") {
-	db = new PrismaClient();
+	db = new PrismaClient().$extends(withAccelerate());
 } else {
 	if (!global.__db__) {
-		global.__db__ = new PrismaClient();
+		global.__db__ = new PrismaClient().$extends(withAccelerate());
 	}
 	db = global.__db__;
 	db.$connect();
 }
 
 export { db };
+
