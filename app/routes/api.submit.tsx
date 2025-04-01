53a71cda88ee0425f17feac1b25d6d52a8675fbf
Matthew Tao <matthew.yingtao@gmail.com>
2025-04-02 00:52:30 +1300

    update response format in api.submit.tsx to use zodTextFormat

--- app/routes/api.submit.tsx
+++ app/routes/api.submit.tsx
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import { zodResponseFormat } from "openai/helpers/zod";
+import { zodTextFormat } from "openai/helpers/zod";
 import { systemPrompt } from "~/data/prompts";
 import { db } from "~/shared/db";
 import { CountryResponse } from "~/shared/zodTypes";
@@ -42,7 +42,9 @@ export async function action({ request }: Route.ActionArgs) {
 				content: `The country is '${country}' and the suggestion is '${suggestion}'`,
 			},
 		],
-		response_format: zodResponseFormat(CountryResponse, "event"),
+		text: {
+			format: zodTextFormat(CountryResponse, "event"),
+		},
 	});
 
 	const res = JSON.parse(completion.choices[0].message.content!);
