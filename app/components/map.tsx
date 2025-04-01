e86b4a0a677344497a4bafd5ef0cd464d660c1ed
Matthew Tao <matthew.yingtao@gmail.com>
2025-04-01 16:54:39 +1300

    add zoom to map

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,4 +1,12 @@
+import { useStore } from "@nanostores/react";
+import { useEffect } from "react";
+import {
+	TransformComponent,
+	TransformWrapper,
+	useControls,
+} from "react-zoom-pan-pinch";
 import { mapPaths } from "~/data/countries";
+import { $currentCountry } from "~/shared/store";
 
 export default function Map({
 	highlightCountry,
@@ -6,25 +14,58 @@ export default function Map({
 	highlightCountry: string;
 }) {
 	return (
-		<svg
-			fill="#ececec"
-			stroke="black"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth=".3"
-			version="1.2"
-			viewBox="0 0 2000 857"
-			className="h-full w-full"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			{mapPaths.map(({ path, name }) => (
-				<path
-					key={path}
-					d={path}
-					fill={highlightCountry === name ? "#AF94E6" : "black"}
-					stroke={highlightCountry === name ? "#4254A0" : "white"}
-				/>
-			))}
-		</svg>
+		<TransformWrapper centerOnInit={true} smooth={false}>
+			<TransformComponent
+				wrapperStyle={{
+					height: "100%",
+					width: "100%",
+				}}
+				contentStyle={{
+					height: "100%",
+					width: "100%",
+				}}
+			>
+				<MapController />
+				<svg
+					fill="#ececec"
+					stroke="black"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth=".3"
+					version="1.2"
+					viewBox="0 0 2000 857"
+					className="h-full w-full"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					{mapPaths.map(({ path, name }) => (
+						<path
+							key={path}
+							d={path}
+							name={name}
+							fill={highlightCountry === name ? "#AF94E6" : "black"}
+							stroke={highlightCountry === name ? "#4254A0" : "white"}
+						/>
+					))}
+				</svg>
+			</TransformComponent>
+		</TransformWrapper>
 	);
+}
+
+function MapController() {
+	const country = useStore($currentCountry);
+	const { zoomToElement } = useControls();
+
+	useEffect(() => {
+		if (!country) return;
+
+		const countryEl = document.querySelector(
+			`[name="${country}"]`
+		) as HTMLElement;
+		if (countryEl) {
+			zoomToElement(countryEl, 2.5);
+		}
+	});
+
+	return <></>;
 }
