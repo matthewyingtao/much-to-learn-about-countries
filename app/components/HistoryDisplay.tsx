427e04a5a4ca4f876462aced038658b2f2e1eb48
Matthew Tao <matthew.yingtao@gmail.com>
2025-05-03 11:35:27 +1200

    refactor: update HistoryDisplay component structure and improve motion handling

--- app/components/HistoryDisplay.tsx
+++ app/components/HistoryDisplay.tsx
@@ -12,19 +12,26 @@ export default function HistoryDisplay() {
 	const history = useStore($history);
 
 	return (
-		<motion.div className="flex flex-col-reverse gap-y-4">
+		<div className="flex flex-col-reverse gap-y-4">
 			{history.map(({ country, guesses }, index) => {
 				// if there are no guesses, don't display the country
 				if (guesses.length === 0) return null;
-
 				return (
-					<div key={country}>
-						<h3>{country}</h3>
+					<motion.div
+						layout
+						initial={{ opacity: 0, y: 8 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ type: "spring", bounce: 0, duration: 0.5 }}
+						key={country}
+					>
+						<h3 className="-z-10 mb-2 translate-y-4 mask-b-from-0 text-5xl leading-[0.85] tracking-wide text-black/30 uppercase">
+							{country}
+						</h3>
 						<div className="flex flex-col-reverse gap-y-2">
 							{guesses.map(({ pass, response, suggestion }, i) => (
 								<motion.div
-									layout="position"
-									key={index}
+									layout
+									key={`${country}-${i}`}
 									className="relative grid grid-cols-[15ch_1fr] items-center gap-6"
 									initial={{ opacity: 0, y: 8, filter: "blur(2px)" }}
 									animate={{
@@ -43,15 +50,10 @@ export default function HistoryDisplay() {
 										},
 									}}
 								>
-									<motion.div
-										className="absolute top-0 -left-3 h-full w-1 rounded-full"
-										animate={{
+									<div
+										className="absolute top-0 -left-3 h-full w-1 rounded-full transition-all duration-150 ease-in-out"
+										style={{
 											backgroundColor: getHistoryCardColor(pass),
-										}}
-										transition={{
-											type: "tween",
-											duration: 0.25,
-											ease: "easeInOut",
 										}}
 									/>
 									<p className="w-[15ch] flex-1">{suggestion}</p>
@@ -59,10 +61,10 @@ export default function HistoryDisplay() {
 								</motion.div>
 							))}
 						</div>
-					</div>
+					</motion.div>
 				);
 			})}
 			<h3 className="text-2xl">History</h3>
-		</motion.div>
+		</div>
 	);
 }
