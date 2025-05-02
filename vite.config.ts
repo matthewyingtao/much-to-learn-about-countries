822deea5bcd186aa43104017e7ba02eecabc594e
Matthew Tao <matthew.yingtao@gmail.com>
2025-05-02 16:47:11 +1200

    Add MillionLint plugin to Vite configuration for improved linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import netlifyPlugin from '@netlify/vite-plugin-react-router';
+import MillionLint from "@million/lint";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+	plugins: [
+		MillionLint.vite({
+			enabled: true,
+		}),
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+		netlifyPlugin(),
+	],
 });
