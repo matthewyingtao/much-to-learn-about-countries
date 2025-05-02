acf5a8eb0bf0632a4551b07e97ca6d62fa7b25b8
Matthew Tao <matthew.yingtao@gmail.com>
2025-05-02 17:01:09 +1200

    refactor: remove MillionLint plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import MillionLint from "@million/lint";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,13 +5,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		MillionLint.vite({
-			enabled: true,
-		}),
-		tailwindcss(),
-		reactRouter(),
-		tsconfigPaths(),
-		netlifyPlugin(),
-	],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
