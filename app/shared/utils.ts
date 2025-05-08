e24c62a6c88e8fad384c2d956cbe012991f4ee69
Matthew Tao <matthew.yingtao@gmail.com>
2025-05-08 15:24:13 +1200

    refactor: add usePrevious hook for tracking previous state values

--- app/shared/utils.ts
+++ app/shared/utils.ts
@@ -1,4 +1,15 @@
+import { useEffect, useRef } from "react";
 export function randomChoice<T>(arr: T[]): T {
 	const i = Math.floor(Math.random() * arr.length);
 	return arr[i];
 }
+
+function usePrevious<T>(value: T): T | undefined {
+	const ref = useRef<T>(undefined);
+	useEffect(() => {
+		ref.current = value;
+	}, [value]);
+	return ref.current;
+}
+
+export default usePrevious;
